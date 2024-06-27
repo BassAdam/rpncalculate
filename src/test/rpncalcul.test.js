d0b150a746e8e18f1e6cc2e8f0c5111fd43d8112
Adam R <ramoukiadam@gmail.com>
2024-06-27 10:31:02 +0200

    ajout des test unitaire

---
+++ src/test/rpncalcul.test.js
@@ -0,0 +1,31 @@
+
+
+import { RPNCal } from "../main/rpn-calculator";
+
+describe("RPN Calculator", () => {
+  test("evaluates '20 5 /' to 4", () => {
+    expect(RPNCal("20 5 /")).toBe(4);
+  });
+
+  test("evaluates '4 2 + 3 -' to 3", () => {
+    expect(RPNCal("4 2 + 3 -")).toBe(3);
+  });
+
+  test("evaluates '3 5 8 * 7 + *' to 141", () => {
+    expect(RPNCal("3 5 8 * 7 + *")).toBe(141);
+  });
+
+  test("evaluates '9 SQRT' to 3", () => {
+    expect(RPNCal("9 SQRT")).toBe(3);
+  });
+
+  test("evaluates '5 3 4 2 9 1 MAX' to 9", () => {
+    expect(RPNCal("5 3 4 2 9 1 MAX")).toBe(9);
+  });
+
+  test("evaluates '4 5 MAX 1 +' to 6", () => {
+    expect(RPNCal("4 5 MAX 1 +")).toBe(6);
+  });
+});
+      
+
